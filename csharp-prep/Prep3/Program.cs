487dc129a24489ce93723540a330e1d9ceff1272
Rodrigo <rodri.salas@hotmail.com>
2023-01-10 22:36:20 -0300

    Prep 3. completed with core & stretch activities

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,8 +4,113 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number?");
-        string user_number = Console.ReadLine();
-        int magicNumber = int.Parse(user_number);
+        {/*Core requirement # 1
+        Console.Write("What is the magic number?: ");
+        string hiddenNumber = Console.ReadLine();
+        int magicNumber = int.Parse(hiddenNumber);
+        
+        Console.Write("What is your guess?: ");
+        string chooseNumber = Console.ReadLine();
+        int userNumber = int.Parse(chooseNumber);
+
+        if (magicNumber == userNumber)
+        {
+            Console.WriteLine("YAY! you found the magic number!");
+        }
+        else if (userNumber > magicNumber)
+        {
+            Console.Write("Lower");
+        }
+        else
+        {
+            Console.Write("Higher");
+        */
+        }
+
+        {/*Core requirement #2
+        
+        int userNumber = 0;
+
+        Console.Write("What is the magic number?: ");
+        string hiddenNumber = Console.ReadLine();
+        int magicNumber = int.Parse(hiddenNumber);
+        
+        while (magicNumber != userNumber)
+        {
+            Console.Write("What is your guess?: ");
+            string chooseNumber = Console.ReadLine();
+            userNumber = int.Parse(chooseNumber);
+
+            if (magicNumber == userNumber)
+            {
+                Console.WriteLine("YAY! you found the magic number!");
+            }
+            else if (userNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        */}
+    
+        
+        /*Core requirement #3*/
+
+        int userNumber = -1;
+        int guesses = 1;
+
+        Random rnd = new Random();
+
+        int hiddenNumber = (rnd.Next(1, 100)); // returns random integers between 1 and 100
+        
+        string continueGame = "yes";
+        while (continueGame == "yes")
+        {
+            
+            while (hiddenNumber != userNumber)
+            {
+                Console.Write("What is your guess?: ");
+                string chooseNumber = Console.ReadLine();
+                userNumber = int.Parse(chooseNumber);
+
+                if (hiddenNumber == userNumber && guesses == 1)
+                {
+                    Console.WriteLine("YAY! you found the magic number in your first try!");
+                }
+                else if (hiddenNumber == userNumber && guesses != 1)
+                {
+                    Console.WriteLine($"YAY! you found the magic number in your try #{guesses}!");
+                }
+                else if (userNumber > hiddenNumber)
+                {
+                    Console.WriteLine("Lower");
+                    guesses += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                    guesses += 1;
+                }
+                
+            }
+
+            Console.Write("Would you like to continue? (yes/no): ");
+            string userInput = Console.ReadLine();
+            if (userInput != continueGame)
+            {
+                continueGame = "no";
+                Console.WriteLine("Thanks for playing.");
+            }
+            else
+            {
+                rnd = new Random();
+                hiddenNumber = (rnd.Next(1, 100));
+                guesses = 1;
+            }
+
+        }
     }
 }

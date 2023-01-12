53d9ae8397c618cce7e2a65ea5cf6950e6290d3a
Rodrigo <rodri.salas@hotmail.com>
2023-01-12 14:43:35 -0300

    Program that returns the square number is finished

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayProgram();
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userNumber = Console.ReadLine();
+        int number = int.Parse(userNumber);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayProgram()
+    {
+        DisplayWelcome();
+        string name = PromptUserName();
+
+        int number = PromptUserNumber();
+        
+        int squareNum = SquareNumber(number);
+        Console.WriteLine($"{name}, the square of your number is {squareNum}.");
+    }
+    
 }
